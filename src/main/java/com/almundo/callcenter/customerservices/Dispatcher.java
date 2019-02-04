961885b169fba781bec46f4a2dab732ef04667ef
jpoh97 <jpoh97@gmail.com>
2019-02-03 22:18:23 -0500

    Added singleton pattern

--- src/main/java/com/almundo/callcenter/customerservices/Dispatcher.java
+++ src/main/java/com/almundo/callcenter/customerservices/Dispatcher.java
@@ -1,4 +1,15 @@
 package com.almundo.callcenter.customerservices;
 
 public class Dispatcher {
+
+    private static Dispatcher dispatcher;
+
+    private Dispatcher() {}
+
+    public static Dispatcher getSingletonInstance() {
+        if (dispatcher == null) dispatcher = new Dispatcher();
+        return dispatcher;
+    }
+
+
 }
