93d99d75ad9f110364602cbe4043f7bf47a35da0
jpoh97 <jpoh97@gmail.com>
2019-02-04 00:20:39 -0500

    Added javadoc

--- src/main/java/com/almundo/callcenter/customerservices/Dispatcher.java
+++ src/main/java/com/almundo/callcenter/customerservices/Dispatcher.java
@@ -7,6 +7,13 @@ import org.apache.log4j.Logger;
 import java.util.concurrent.PriorityBlockingQueue;
 import java.util.concurrent.TimeUnit;
 
+/**
+ * Dispatcher tiene la funcion de coordinar cada las llamadas que entran. Esta clase implementa el patron Singleton.
+ * Se utilizo un PriorityBlockingQueue para manejar el orden en el cual se reciben las llamadas dependiendo del cargo.
+ * Con employees.poll() se asegura que si no hay empleados disponibles, es decir que los 10 empleados estan ocupados
+ * y llega una onceava llamada, el sistema esperara hasta que se encuentre un empleado disponible, ya que el
+ * PriorityBlockingQueue bloquea el hilo hasta que exista un empleado a retornar
+ */
 public class Dispatcher {
 
     private static final String MENSAJE_LLAMADA_RECIBIDA = "La llamada #%d sera contestada por %s";
@@ -31,12 +38,12 @@ public class Dispatcher {
 
     public void dispatchCall(Call call) {
         try {
-            Employee employee = employees.take();
+            Employee employee = employees.poll();
             logger.info(String.format(MENSAJE_LLAMADA_RECIBIDA, call.getId(), employee.getName()));
             TimeUnit.SECONDS.sleep(call.getDuration());
             logger.info(String.format(MENSAJE_LLAMADA_TERMINADA, call.getId(), call.getDuration()));
             employees.offer(employee);
-        } catch (InterruptedException e) {
+        } catch (InterruptedException | NullPointerException e) {
             logger.error(e.getMessage(), e);
         }
     }
